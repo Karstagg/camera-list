3c56ba5241b2d2ed156b83e46ebd307ae0b71934
matthewFisher <m.r.f.48215@gmail.com>
2019-06-25 19:15:27 +0900

    styling cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,59 @@
-import React from 'react'
-import Styled from 'styled-components'
+import React from "react"
+import Styled from "styled-components"
 
 const Card = Styled.div`
   width: 300px;
   height: 300px; 
   margin: 0 auto;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `
 const CardBody = Styled.div`
   width: 100%;
   height: 80%; 
   background-size: cover;
   background-position: center;
+  border-radius: 15px 15px 0px 0px;
 `
 const CardFooter = Styled.div`
   width: 100%;
   height: 20%; 
   background-color: black;
   color: white;
+  border-radius: 0px 0px 15px 15px;
+  text-align: left;
+    display: grid;
+  grid-template-columns: 10% 90%;
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+  width: 100%;
 `
+const CardIndicator = Styled.div`
+ margin-top: 5px;
+ height: 75%;
+ width: 10px;
+ margin-left 10px;
+ border-radius: 15px;
+`
+const FooterText = Styled.div`
+  margin-top: 5px;
+`
+
 function Nav(props) {
-  let deviceInfo = props.device[0];
-  let deviceStatus = props.device[1];
+  let deviceInfo = props.device[0]
+  let deviceStatus = props.device[1]
   return <Card>
-    <CardBody style={deviceStatus.active ? {backgroundImage: `url(${deviceStatus.thumbnail})`} : {backgroundImage: `url(${deviceStatus.thumbnail})`, filter: 'opacity(0.6)'}}/>
-    <CardFooter>{deviceInfo.name}</CardFooter>
+    <CardBody style={deviceStatus.active ? { backgroundImage: `url(${deviceStatus.thumbnail})` } : {
+      backgroundImage: `url(${deviceStatus.thumbnail})`,
+      filter: "opacity(0.6)",
+    }}/>
+    <CardFooter>
+      <CardIndicator style={deviceStatus.active ? {backgroundColor: '#1DD387'} : {backgroundColor: '#CA6060'}}/>
+      <FooterText>
+        <div>{deviceStatus.active ? "ACTIVE" : "INACTIVE"}</div>
+        <div>{deviceInfo.name}</div>
+      </FooterText>
+    </CardFooter>
   </Card>
 }
 
